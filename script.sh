4379187f4046f3dd6cc0dd66cc47b3c7ca5ab593
orangecrow <34030880+orangecrow@users.noreply.github.com>
2019-09-12 15:09:31 +0200

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,7 @@
+cp .Xresources ~/
+git clone https://github.com/muennich/urxvt-perls.git
+cp urxvt-pearls/keyboard-select ~/.urxvt/ext/
+cp urxvt-pearls/deprecated/clipboard ~/.urxvt/ext/  
+cp urxvt-pearls/deprecated/url-select ~/.urxvt/ext/    
+git clone https://github.com/majutsushi/urxvt-font-size.git
+cp urxvt-font-size/font-size ~/.urxvt/ext/
